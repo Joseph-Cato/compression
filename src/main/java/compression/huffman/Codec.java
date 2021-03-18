bc659d683cbea05d68ff0a39574da631ba1c1fee
Joseph Cato <joejcato@outlook.com>
2021-03-18 16:11:56 +0000

    Codec
    Decode implemented

--- src/main/java/compression/huffman/Codec.java
+++ src/main/java/compression/huffman/Codec.java
@@ -22,17 +22,34 @@ public class Codec {
 
     static String decode(Node tree, String text) {
 
+        // Get BidiMap of codes and characters
         BidiMap<Character, String> codes = Tree.getCodes(tree);
 
+        // Create StringBuilder objects for the final output and the current code being checked
         StringBuilder output = new StringBuilder();
+        StringBuilder code = new StringBuilder();
 
-        return "";
+        // Iterate through encoded text and append char's to output
+        for (int i = 0; i < text.length(); i++) {
 
-    }
+            // Add next value of encoded text to current code
+            code.append(text.charAt(i));
 
-    private static String checkCode(String code, HashMap<Character, String> codes) {
+            try {
+                // Get key char from BidiMap according to code value
+                // If key is not found the BidiMap methods will assign this char to null, throwing an Exception
+                char c = codes.getKey(code.toString());
 
-        return "";
+                // code will be reset and char will be appended to output
+                code = new StringBuilder();
+                output.append(c);
+
+            } catch (Exception e) {
+                // If key is not found loop will continue
+            }
+        }
+
+        return output.toString();
     }
 
 }
