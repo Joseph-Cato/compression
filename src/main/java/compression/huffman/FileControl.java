d33cf2a06cfb72a8c553489b3b808fcd8f701384
Joseph Cato <joejcato@outlook.com>
2021-03-19 01:12:25 +0000

    Read binary added

--- src/main/java/compression/huffman/FileControl.java
+++ src/main/java/compression/huffman/FileControl.java
@@ -25,4 +25,30 @@ public class FileControl {
             e.printStackTrace();
         }
     }
+
+    public static String readBinary(){
+        Path path = Paths.get("compressed.bin");
+        String binary = null;
+        try {
+            byte [] fileBytes = Files.readAllBytes(path);
+            int finalBits = fileBytes[fileBytes.length-1];
+            String[] binaryList = new String[fileBytes.length-1];
+            for (int i = 0; i<fileBytes.length-1; i++){
+                int formattedByte = fileBytes[i] & 0xff;
+                binaryList[i] = Integer.toBinaryString(formattedByte);
+            }
+            String lastByte = binaryList[binaryList.length-1];
+            for (int i = 0; i<finalBits; i++){
+                lastByte = lastByte.substring(0,lastByte.length()-1);
+            }
+            binaryList[binaryList.length-1] = lastByte;
+            binary = binaryList[0];
+            for (int i = 1; i<binaryList.length; i++){
+                binary += binaryList[i];
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return binary;
+    }
 }
